21c95dfc7fca343c773d26f403297f25288968bf
iping <iping>
2018-01-13 16:44:55 +0800

    testing

--- indexTest.js
+++ indexTest.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path')
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.json({a:1}))
-  .listen(5000, () => console.log(`Listening on`))
+  .listen(PORT, () => console.log(`Listening on`))
