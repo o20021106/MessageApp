062fd64b3cbb4375716a49586038f8ad985cf687
iping <iping>
2018-01-29 10:42:39 +0800

    heroku testing

--- routers/index.js
+++ routers/index.js
@@ -7,7 +7,7 @@ var searchController = require('../controllers/search.js');
 var config = require("../config/main");
 const PORT = process.env.PORT || 6000;
 
-const hostname = process.env.host || `localhost:${PORT}`;
+const hostname = process.env.host || `localhost:${PORT}/`;
 const hostRe = 'https://'+hostname;
 
 
@@ -36,39 +36,39 @@ router.get('/authenticate', function(req,res){
 var passport = require('passport');  
 
 
-router.get('/testing', passport.authenticate('jwt', { session: false,failureRedirect:hostRe+'/login' }), function(req, res){
+router.get('/testing', passport.authenticate('jwt', { session: false,failureRedirect:hostRe+'login' }), function(req, res){
   console.log(req.user.email);
   res.json("Success! You can not see this without a token");
 });
-router.get("/secrete", passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'/login'}), function(req, res){
+router.get("/secrete", passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'login'}), function(req, res){
 
 	console.log(req.user.email);
   res.json("Success! You can not see this without a token");
 });
 
-router.get('/searchUser', passport.authenticate('jwt', {session: false ,failureRedirect:hostRe+'/login'}), searchController.searchUser.get);
-router.get('/getRecipients', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'/login'}), chatController.getRecipients);
-router.get('/getConversations', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'/login'}), chatController.getConversations);
-router.get('/getConversation/:conversationId', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'/login'}), chatController.getConversation);
-router.get('/getConversationByRecipientId/:recipientId', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'/login'}), chatController.getConversationByRecipientId);
+router.get('/searchUser', passport.authenticate('jwt', {session: false ,failureRedirect:hostRe+'login'}), searchController.searchUser.get);
+router.get('/getRecipients', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'login'}), chatController.getRecipients);
+router.get('/getConversations', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'login'}), chatController.getConversations);
+router.get('/getConversation/:conversationId', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'login'}), chatController.getConversation);
+router.get('/getConversationByRecipientId/:recipientId', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'login'}), chatController.getConversationByRecipientId);
 
-router.get('/recipient/:recipientId', passport.authenticate('jwt', { session: false , failureRedirect:hostRe+'/login'}),chatController.chatLoad, chatController.message.get);
+router.get('/recipient/:recipientId', passport.authenticate('jwt', { session: false , failureRedirect:hostRe+'login'}),chatController.chatLoad, chatController.message.get);
 
-router.get('/message', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'/login'}), chatController.message.get);
+router.get('/message', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'login'}), chatController.message.get);
 
 
-router.get('/editProfile', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'/login'}), userController.editProfileTesting.get);
+router.get('/editProfile', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'login'}), userController.editProfileTesting.get);
 router.get('/registerLogin', function(req,res){
     res.sendFile(path.join(__dirname, '/../registerLogin.html'));
 })
-router.get('/', passport.authenticate('jwt', {failureRedirect:hostRe+'/loginhaha',session: false }),function(req,res){
+router.get('/', passport.authenticate('jwt', {failureRedirect:hostRe+'login',session: false }),function(req,res){
     res.redirect('https://'+req.headers.host+'/message')
 })
 router.post('/register',userController.register.post);
-router.post('/editProfile', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'/login'}), userController.editProfile.post);
+router.post('/editProfile', passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'login'}), userController.editProfile.post);
 
 router.post('/login',userController.login.post);
-router.post('/newMessage',passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'/login'}), chatController.newConversation);
+router.post('/newMessage',passport.authenticate('jwt', { session: false ,failureRedirect:hostRe+'login'}), chatController.newConversation);
 //router.post('/authenticate',userController.authenticate.post);
 
 module.exports = router;
